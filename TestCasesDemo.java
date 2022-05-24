d279e49035f900c048174935caaa9abe29332b5d
Sofie22 <99754211+Sofie22@users.noreply.github.com>
2022-05-24 10:20:25 -0500

    add changes

--- TestCasesDemo.java
+++ TestCasesDemo.java
@@ -1,6 +1,8 @@
 public class TestCasesDemo {
     public static void main(String[] args) {
         System.out.println("test cases branches");
+        System.out.println("new changes");
+
 
 
     }
